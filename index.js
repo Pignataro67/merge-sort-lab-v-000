8a2fb7d46fa54c7a8733ad0f36b3a04eff793475
Pat  Pignataro <pat6284@msn.com>
2019-04-05 13:30:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ function findMinAndRemoveSorted(array){
   return array.shift();
 }
 
-
-// function findMinAndRemoveSorted(array){
-//   return array.shift();
-// }
-
+function merge(array1, array2){
+  let sorted = []
+  while (array1.length !== 0 && array2.length !== 0){
+    if (array1[0] < array2[0]){
+      sorted.push(findMinAndRemoveSorted(array1))
+    } else {
+      sorted.push(findMinAndRemoveSorted(array2))
+    }
+  }
+  retrun sorted.concat(array1).concat(array2);
+}
 // function merge(array1, array2){
 //   let sorted = []
 //   while (array1.length != 0 && array2.length != 0){
